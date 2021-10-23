1ae7351b634622b2dc37b7d3f6e2549b8eec1928
Dániel Árvai <arvai96@gmail.com>
2021-10-23 11:55:26 +0200

    fix(model): fix incorrect generic types on SaleRepository

--- src/main/java/hu/uni/eku/tzs/dao/SaleRepository.java
+++ src/main/java/hu/uni/eku/tzs/dao/SaleRepository.java
@@ -1,7 +1,7 @@
 package hu.uni.eku.tzs.dao;
 
-import hu.uni.eku.tzs.dao.entity.CustomerEntity;
+import hu.uni.eku.tzs.dao.entity.SaleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SaleRepository extends JpaRepository<CustomerEntity, String> {
+public interface SaleRepository extends JpaRepository<SaleEntity, Integer> {
 }
