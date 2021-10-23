36cb34b1f9d0ff73751b40e0a0ec99ed33cea438
Dániel Árvai <arvai96@gmail.com>
2021-10-23 10:22:44 +0200

    fix(model): use single class imports instead of * in Product

--- src/main/java/hu/uni/eku/tzs/model/Product.java
+++ src/main/java/hu/uni/eku/tzs/model/Product.java
@@ -1,6 +1,10 @@
 package hu.uni.eku.tzs.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
