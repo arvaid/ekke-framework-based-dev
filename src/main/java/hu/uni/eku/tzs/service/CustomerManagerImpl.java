b2caaf05de03273b54e00803fd9c6163d4730718
Dániel Árvai <arvai96@gmail.com>
2021-10-23 11:24:40 +0200

    feat(service): add CustomerManagerImpl

---
+++ src/main/java/hu/uni/eku/tzs/service/CustomerManagerImpl.java
@@ -0,0 +1,82 @@
+package hu.uni.eku.tzs.service;
+
+import hu.uni.eku.tzs.dao.CustomerRepository;
+import hu.uni.eku.tzs.dao.entity.CustomerEntity;
+import hu.uni.eku.tzs.dao.entity.EmployeeEntity;
+import hu.uni.eku.tzs.model.Customer;
+import hu.uni.eku.tzs.model.Employee;
+import hu.uni.eku.tzs.service.exceptions.CustomerAlreadyExistsException;
+import hu.uni.eku.tzs.service.exceptions.CustomerNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerManagerImpl implements CustomerManager {
+
+    private final CustomerRepository customerRepository;
+
+    private static Customer convertCustomerEntity2Model(CustomerEntity customerEntity) {
+        return new Customer(
+                customerEntity.getId(),
+                customerEntity.getFirstName(),
+                customerEntity.getMiddleInitial(),
+                customerEntity.getLastName()
+        );
+    }
+
+    private static CustomerEntity convertCustomerModel2Entity(Customer customer) {
+        return CustomerEntity.builder()
+                .id(customer.getId())
+                .firstName(customer.getFirstName())
+                .middleInitial(customer.getMiddleInitial())
+                .lastName(customer.getLastName())
+                .build();
+    }
+
+    @Override
+    public Customer record(Customer customer) throws CustomerAlreadyExistsException {
+        if (customerRepository.findById(customer.getId()).isPresent()) {
+            throw new CustomerAlreadyExistsException();
+        }
+        CustomerEntity customerEntity = customerRepository.save(
+                CustomerEntity.builder()
+                        .id(customer.getId())
+                        .firstName(customer.getFirstName())
+                        .middleInitial(customer.getMiddleInitial())
+                        .lastName(customer.getLastName())
+                        .build()
+        );
+        return convertCustomerEntity2Model(customerEntity);
+    }
+
+    @Override
+    public Customer readById(int id) throws CustomerNotFoundException {
+        Optional<CustomerEntity> entity = customerRepository.findById(id);
+        if (entity.isEmpty()) {
+            throw new CustomerNotFoundException(String.format("Cannot find customer with ID %d", id));
+        }
+        return convertCustomerEntity2Model(entity.get());
+    }
+
+    @Override
+    public Collection<Customer> readAll() {
+        return customerRepository.findAll().stream().map(CustomerManagerImpl::convertCustomerEntity2Model)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Customer modify(Customer customer) {
+        CustomerEntity entity = convertCustomerModel2Entity(customer);
+        return convertCustomerEntity2Model(customerRepository.save(entity));
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        customerRepository.delete(convertCustomerModel2Entity(customer));
+    }
+}
--- src/main/java/hu/uni/eku/tzs/service/exceptions/CustomerNotFoundException.java
+++ src/main/java/hu/uni/eku/tzs/service/exceptions/CustomerNotFoundException.java
@@ -1,4 +1,7 @@
 package hu.uni.eku.tzs.service.exceptions;
 
 public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
 }
