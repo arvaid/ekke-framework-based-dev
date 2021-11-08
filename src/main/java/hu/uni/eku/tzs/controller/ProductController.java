536ea552ac6bf5eebc92e0036b1efbc88bd4bde9
Dániel Árvai <arvai96@gmail.com>
2021-11-08 20:52:32 +0100

    fix(controller): fix checkstyle violations

--- src/main/java/hu/uni/eku/tzs/controller/ProductController.java
+++ src/main/java/hu/uni/eku/tzs/controller/ProductController.java
@@ -10,7 +10,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
@@ -47,8 +54,6 @@ public class ProductController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
-
-
 
     @ApiOperation("Delete")
     @DeleteMapping(value = {"", "/"})
