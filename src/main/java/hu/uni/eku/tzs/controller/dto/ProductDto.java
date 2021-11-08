df1499a5bc4dc8e6081bf9472e860c088bd86ee8
Dániel Árvai <arvai96@gmail.com>
2021-11-08 19:49:56 +0100

    feat(controller): add ProductDto

---
+++ src/main/java/hu/uni/eku/tzs/controller/dto/ProductDto.java
@@ -0,0 +1,22 @@
+package hu.uni.eku.tzs.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+
+    private int id;
+
+    @NotBlank(message = "name cannot be empty")
+    private String name;
+
+    private double price;
+}
